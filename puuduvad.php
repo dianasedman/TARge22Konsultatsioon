f59d67f32cfb8c04900a3694426589cbc57c001e
Doris Valis <valisdoris@gmail.com>
2023-10-23 21:27:18 +0300

    puuduvad fail

--- puuduvad.php
+++ puuduvad.php
@@ -1,16 +1,42 @@
 <?php
+require($_SERVER["DOCUMENT_ROOT"] . "/../config.php");
+global $yhendus;
+
+$query = "SELECT o.opetajanimi
+          FROM opetaja o
+          WHERE o.id NOT IN (
+            SELECT k.opetaja
+            FROM konsultatsioon k
+            GROUP BY k.opetaja
+            HAVING COUNT(*) >= 2
+          )";
+$result = mysqli_query($yhendus, $query);
+$nimed = array();
+while ($row = mysqli_fetch_assoc($result)) {
+  $nimed[] = $row["opetajanimi"];
+}
+
+?>
+<?php
 require("header.php");
 ?>
 
 <body>
   <h1>Puuduvad konsultatsiooni ajad</h1>
   <h3>2023 - 2024. õa</h3>
+
   <div class="teacher-list">
-    <ol>
-      <li>Kukk</li>
-      <li>Lehm</li>
-      <li>Loots</li>
-    </ol>
+    <?php
+    if (!empty($nimed)) {
+      echo "<ul>";
+      foreach ($nimed as $nimi) {
+        echo "<li>$nimi</li>";
+      }
+      echo "</ul>";
+    } else {
+      echo "Kõik õpetajad vastavad tingimusele.";
+    }
+    ?>
   </div>
 
 </body>
