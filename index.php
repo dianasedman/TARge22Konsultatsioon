6805682afd4fb3d68f29f31d7b7b1c0ebb227bdb
Doris Valis <valisdoris@gmail.com>
2023-10-27 09:14:27 +0300

    index edit

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ require("header.php");
         echo "<h2>Otsingu tulemused aine kohta: " . htmlspecialchars($searchTerm) . "</h2>";
 
         while ($kask->fetch()) {
-          echo "<a href='?id=$id' style='color: #d3d3d3'>" . htmlspecialchars($aine) . "</a>";
+          echo "<a href='?aine=$aine' style='color: #d3d3d3'>" . htmlspecialchars($aine) . "</a>";
           echo "<br>";
         }
 
